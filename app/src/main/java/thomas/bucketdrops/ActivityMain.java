d084f3a541f470c8a82d17e7125cc848c2b375b4
Thedruids <The_only_0ne@hotmail.com>
2016-03-16 13:22:37 +0000

    3rd Commit, added some more code and files, tidied things up a little

--- app/src/main/java/thomas/bucketdrops/ActivityMain.java
+++ app/src/main/java/thomas/bucketdrops/ActivityMain.java
@@ -1,13 +1,40 @@
 package thomas.bucketdrops;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
 
 public class ActivityMain extends AppCompatActivity {
+
+    Toolbar mToolbar;
+    Button mBtnAdd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        mBtnAdd = (Button) findViewById(R.id.btn_add);
+        mBtnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        setSupportActionBar(mToolbar);
+        initBackgroundImage();
     }
-}
+
+    private void initBackgroundImage(){
+        ImageView background = (ImageView) findViewById(R.id.iv_background);
+        assert background != null : "Background image not found";
+        Glide.with(this).load(R.drawable.background).centerCrop().into(background);
+        }
+    }
+
