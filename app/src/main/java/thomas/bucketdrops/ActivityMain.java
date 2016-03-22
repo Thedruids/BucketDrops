f0c68ccd345aca911ab0969b003b7fa18060eff6
Thedruids <The_only_0ne@hotmail.com>
2016-03-22 15:26:08 +0000

    6th commit, began adding the RecyclerView, updated Gradle with the appropriate dependencies and stuff. Also added some image files.

--- app/src/main/java/thomas/bucketdrops/ActivityMain.java
+++ app/src/main/java/thomas/bucketdrops/ActivityMain.java
@@ -2,6 +2,7 @@ package thomas.bucketdrops;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
@@ -13,6 +14,7 @@ public class ActivityMain extends AppCompatActivity {
 
     Toolbar mToolbar;
     Button mBtnAdd;
+    RecyclerView mRecylcer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,16 +22,23 @@ public class ActivityMain extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         mToolbar = (Toolbar) findViewById(R.id.toolbar);
         mBtnAdd = (Button) findViewById(R.id.btn_add);
+        mRecylcer = (RecyclerView) findViewById(R.id.rv_drops);
+
         mBtnAdd.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                showDialogAdd();
             }
         });
 
         setSupportActionBar(mToolbar);
         //This is the method call for the glide background initialisation.
         initBackgroundImage();
+    }
+
+    private void showDialogAdd() {
+        DialogAdd dialogAdd = new DialogAdd();
+        dialogAdd.show(getSupportFragmentManager(), "Add");
     }
 
     //When the size of the image exceeds the RAM of the emulator, it is necessary to use a program like Glide to load the image otherwise the program will crash
---
+++ app/src/main/java/thomas/bucketdrops/DialogAdd.java
@@ -0,0 +1,84 @@
+package thomas.bucketdrops;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.DialogFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.ImageButton;
+
+import io.realm.Realm;
+import io.realm.RealmConfiguration;
+import thomas.bucketdrops.beans.Drop;
+
+
+public class DialogAdd extends DialogFragment{
+
+    //The add it fragment contains 4 variables which will be used to give functionality to the views in the dialog_add.xml file.
+
+    private ImageButton mBtnClose;
+    private EditText mInputWhat;
+    private DatePicker mInputWhen;
+    private Button mButtonAdd;
+
+
+        //An empty constructor is required when creating fragments.
+    public DialogAdd() {
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.dialog_add, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        //Java needs the context in order to find view by id. The context can be returned through the view object.
+        mInputWhen = (DatePicker) view.findViewById(R.id.bpv_date);
+        mInputWhat = (EditText) view.findViewById(R.id.et_drop);
+        mButtonAdd = (Button) view.findViewById(R.id.btn_add_it);
+        mBtnClose = (ImageButton) view.findViewById(R.id.btn_close);
+
+        mBtnClose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dismiss();
+            }
+        });
+
+        mButtonAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addAction();
+
+                dismiss();
+            }
+        });
+
+    }
+
+    //TODO process date
+    private void addAction() {
+        String what = mInputWhat.getText().toString();
+        long now = System.currentTimeMillis();
+
+
+
+        RealmConfiguration configuration = new RealmConfiguration.Builder(getActivity()).build();
+        Realm.setDefaultConfiguration(configuration);
+        Realm realm = Realm.getDefaultInstance();
+        Drop drop = new Drop(what, now, 0, false);
+        realm.beginTransaction();
+        realm.copyToRealm(drop);
+        realm.commitTransaction();
+        realm.close();
+
+    }
+}
